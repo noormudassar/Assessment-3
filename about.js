350c624b29b277cd1e93c19c83061db6fdb3c77d
Noor Mudassar <noormudassar97@gmail.com>
2023-07-07 17:16:54 -0400

    part 2 solved

--- about.js
+++ about.js
@@ -1,14 +1,18 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+  evt.preventDefault();
+  alert(`Form submitted successfully`);
+  // console.log('form submit');
 }
 
+function mouseOver(evt) {
+  evt.preventDefault();
+  alert(`You have a really good eye to have found this!`);
+}
 
-let form = document.querySelector('#contact');
+let form = document.querySelector("#contact");
+form.addEventListener("submit", handleSubmit);
 
-form.addEventListener('submit', handleSubmit);
+let image = document.querySelector(`#homeImage`);
+image.addEventListener("mouseover", mouseOver);
